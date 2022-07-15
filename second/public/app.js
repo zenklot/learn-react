ef5bd6136a13ccf75a6c1d802a79a2da622a1b3a
zenklot <zenklot11@gmail.com>
2022-07-15 14:59:04 +0700

    feat: state in react

--- second/public/app.js
+++ second/public/app.js
@@ -1,10 +1,21 @@
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
-function padaSaatDiKlik(msg) {
-  alert(msg);
+function App() {
+  // Manual
+  // const state = React.useState(0)
+  // const count = state[0]
+  // const setCount = state[1]
+  // Automatic
+  const [count, setCount] = React.useState(0);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("button", {
+    onClick: () => {
+      setCount(count - 1);
+    }
+  }, "-"), /*#__PURE__*/React.createElement("span", null, count), /*#__PURE__*/React.createElement("button", {
+    onClick: function () {
+      setCount(count + 1);
+    }
+  }, "+"));
 }
 
-const element = /*#__PURE__*/React.createElement("button", {
-  onClick: padaSaatDiKlik.bind(this, 'cekk')
-}, "Click Me");
-root.render(element);
+root.render( /*#__PURE__*/React.createElement(App, null));
--- second/src/app.js
+++ second/src/app.js
@@ -1,8 +1,29 @@
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
-function padaSaatDiKlik(msg) {
-    alert(msg);
-}
-const element = <button onClick={padaSaatDiKlik.bind(this, 'cekk')}>Click Me</button>
+function App() {
+    // Manual
+    // const state = React.useState(0)
+    // const count = state[0]
+    // const setCount = state[1]
 
-root.render(element);
+    // Automatic
+    const [count, setCount] = React.useState(0)
+
+    return (
+        <>
+            <button onClick={
+                () => {
+                    setCount(count - 1)
+                }
+            }>-</button>
+            <span>{count}</span>
+            <button onClick={
+              function (){
+                setCount(count + 1)
+              }  
+            }>+</button>
+        </>
+    )
+}
+
+root.render(<App />);
